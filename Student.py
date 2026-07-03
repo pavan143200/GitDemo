4cc2f81919e961d9a04ddeca8f4ad0d5e41c43dd
Pavan <pavanfd1504@gmail.com>
2026-07-03 20:29:19 +0530

    Updated now

--- Student.py
+++ Student.py
@@ -1,1 +1,13 @@
 print("student names")
+print("Accc")
+print("badd")
+
+'''create file
+git init
+git status
+git add .
+git commit -m "initial commit"
+modify marks.py
+git add .
+git status
+git commit -m "updated marks"'''
